0ecbaac70e6b9e33e3e93a9124044e97caf1d092
alfredoPark-48 <parkalfredojeonghyun@gmail.com>
2021-09-08 09:56:28 -0500

    Reducers added

--- src/index.js
+++ src/index.js
@@ -38,6 +38,28 @@ const createClaim = (name, amountOfMoneyToCollect) => {
 	};
 };
 
+// Reducer
+const claimsHistory = (oldListOfClaims = [], action) => {
+	if (action.type === "CREATE_CLAIM") {
+		// We care about the form
+		return [...oldListOfClaims, action.payload];
+	}
+
+	// We don't care about the form
+	return oldListOfClaims;
+};
+
+// Reducer
+const accounting = (bagOfMoney = 100, action) => {
+	if (action.type === "CREATE_CLAIM") {
+		return bagOfMoney - action.payload.amountOfMoneyToCollect;
+	} else if (action.type === "CREATE_POLICY") {
+		return bagOfMoney + action.payload.amountOfMoneyToCollect;
+	} else {
+		return bagOfMoney;
+	}
+};
+
 const App = () => {
 	return <div>Hello World! Open console.</div>;
 };
